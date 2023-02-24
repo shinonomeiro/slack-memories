18d75412477c69746097cf5da59fd2929a93476d
Frank <frank.nguyen@reaktor.com>
2023-02-24 23:56:28 +0900

    [TEST] Added some unit tests

---
+++ tests/bot.test.ts
@@ -0,0 +1,82 @@
+import { jest, expect, it, beforeEach, afterEach } from '@jest/globals'
+import { WebClient } from '@slack/web-api';
+import { fromUnixTime } from 'date-fns';
+import { compose } from 'lodash/fp';
+import MemoriesBot from '../src/bot';
+import { getMessagesFor, getPermalinkFor, today } from './mock-data';
+
+jest.mock('@slack/web-api', () => ({
+  WebClient: jest.fn().mockImplementation(() => ({
+    conversations: {
+      history: jest.fn(),
+    },
+    chat: {
+      getPermalink: jest.fn(),
+      postMessage: jest.fn(),
+    },
+  })),
+}));
+
+const service = new WebClient('token');
+let historyMock = service.conversations.history as jest.Mock<typeof service.conversations.history>;
+let getPermalinkMock = service.chat.getPermalink as jest.Mock<typeof service.chat.getPermalink>;
+let postMessageMock = service.chat.postMessage as jest.Mock<typeof service.chat.postMessage>;
+
+beforeEach(() => {
+  historyMock.mockImplementation(async options => {
+    let date = compose(fromUnixTime, parseFloat)(options!.oldest!);
+    return { ok: true, messages: getMessagesFor(date.getFullYear()) };
+  });
+  
+  getPermalinkMock.mockImplementation(async options => {
+    const permalink = getPermalinkFor(options!.message_ts);
+    return { ok: true, permalink: permalink };
+  });
+  
+  postMessageMock.mockImplementation(async () => ({ ok: true }));
+});
+
+afterEach(() => {
+  historyMock.mockReset();
+  getPermalinkMock.mockReset();
+  postMessageMock.mockReset();
+
+});
+
+const bot = new MemoriesBot(service);
+bot.setToday(today)
+
+it("throws if any channel ID is an empty string", async () => {
+  let fromChannel = { id: '', name: '' }
+  let toChannel = { id: '', name: '' }
+
+  await expect(bot.run(toChannel)(fromChannel)).rejects.toThrow();
+});
+
+it("picks the messages with the most reactions for each year and posts them to the target channel", async () => {
+  let fromChannel = { id: 'abc', name: '#from' }
+  let toChannel = { id: 'def', name: '#to' }
+
+  await bot.run(toChannel)(fromChannel);
+  
+  expect(postMessageMock).toHaveBeenCalledTimes(6);
+  expect(postMessageMock).toHaveBeenNthCalledWith(1, { channel: 'def', text: '⭐️⭐️⭐️ Memories of #from for today ⭐️⭐️⭐️' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(2, { channel: 'def', text: '💭 5 years ago, on 2018/01/01... 💭' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(3, { channel: 'def', text: 'link200\nlink100' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(4, { channel: 'def', text: '💭 7 years ago, on 2016/01/01... 💭' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(5, { channel: 'def', text: 'link4\nlink2\nlink1' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(6, { channel: 'def', text: '👋 And that\'s it for today! See you again tomorrow! 👋' });
+});
+
+it('has no memories to post for today', async () => {
+  let fromChannel = { id: 'abc', name: '#from' }
+  let toChannel = { id: 'def', name: '#to' }
+  
+  historyMock.mockImplementation(async () => ({ ok: true }));
+
+  await bot.run(toChannel)(fromChannel);
+  
+  expect(postMessageMock).toHaveBeenCalledTimes(2);
+  expect(postMessageMock).toHaveBeenNthCalledWith(1, { channel: 'def', text: '⭐️⭐️⭐️ Memories of #from for today ⭐️⭐️⭐️' });
+  expect(postMessageMock).toHaveBeenNthCalledWith(2, { channel: 'def', text: '😭 Alas I couldn\'t find any. Come back tomorrow! 😭' });
+});
---
+++ tests/mock-data.ts
@@ -0,0 +1,88 @@
+import { RawMessage } from '../@types/global';
+
+export const today = new Date(2023, 0, 1);
+
+export function getMessagesFor(year: number): RawMessage[] {
+  if (year == 2016) {
+    return [
+      {
+        ts: "1",
+        reactions: [
+          { name: "emojiA", count: 1 },
+          { name: "emojiB", count: 1 },
+        ],
+      },
+      {
+        ts: "2",
+        reactions: [
+          { name: "emojiA", count: 1 },
+          { name: "emojiB", count: 2 },
+        ],
+      },
+      {
+        ts: "3",
+      },
+      {
+        ts: "4",
+        reactions: [
+          { name: "emojiA", count: 4 },
+        ],
+      },
+    ];
+  }
+  
+  if (year == 2017) {
+    return [
+      {
+        ts: "10",
+      },
+      {
+        ts: "20",
+      },
+    ];
+  }
+  
+  if (year == 2018) {
+    return [
+      {
+        ts: "100",
+        reactions: [
+          { name: "emojiA", count: 1 },
+          { name: "emojiB", count: 1 },
+        ],
+      },
+      {
+        ts: "200",
+        reactions: [
+          { name: "emojiA", count: 1 },
+          { name: "emojiB", count: 1 },
+        ],
+      },
+      {
+        ts: "300",
+      },
+    ];
+  }
+  
+  if (year == 2019) {
+    return [];
+  }
+  
+  return [];
+}
+
+export function getPermalinkFor(ts: string) {
+  switch (ts) {
+    case '1': return "link1";
+    case '2': return "link2";
+    case '3': return "link3";
+    case '4': return "link4";
+
+    case '10': return "link10";
+    case '20': return "link20";
+
+    case '100': return "link100";
+    case '200': return "link200";
+    case '300': return "link300";
+  }
+}
